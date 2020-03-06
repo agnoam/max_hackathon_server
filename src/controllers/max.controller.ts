c8425ad56aafc879173be152aac84b7f49deb460
Yehuda Daniel <h10ytguf@gmail.com>
2020-03-06 15:04:30 +0200

    new api functions

--- src/controllers/max.controller.ts
+++ src/controllers/max.controller.ts
@@ -7,4 +7,19 @@ export module MaxRequests {
             forceSoap12Headers: true
         });
     }
+
+    export async function cardActivate(requestData: { AccountId?: Long, ValidThru?: string, SecurityCode?: string }): Promise<any>{
+        const url: string = 'https://webservices.coriunder.cloud/v2/Prepaid.svc';
+        await soap.createClientAsync(url, {
+            forceSoap12Headers: true
+        });
+    }
+
+    export async function cardActivateV2(requestData: { requestParams?: PrePaidRP }): Promise<any> {
+        
+    }
+}
+
+interface PrePaidRP{
+    null: null
 }
