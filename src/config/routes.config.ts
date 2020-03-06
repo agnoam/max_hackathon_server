8223ccfc40d224a32660c21bcb10fcfd5d372e17
agnoam <agnoam.aghai.ooo.123@gmail.com>
2020-03-06 15:00:35 +0200

    max first request returns status 200 !

--- src/config/routes.config.ts
+++ src/config/routes.config.ts
@@ -1,5 +1,6 @@
 import { Application, Request, Response } from "express";
-import { appRouter } from './../routes/app.routes';
+import { appRouter } from '../routes/app.routes';
+import { MaxRequests } from "../controllers/max.controller";
 
 console.log("import routes.config");
 
@@ -8,6 +9,9 @@ export const RoutesConfig = (app: Application) => {
     app
         .use('/app', appRouter)
 
+        .get('/max-try', (req: Request, res: Response) => {
+            MaxRequests.accountAdd({ AccountId: 62525652626659, Currency: '' });
+        })
         .get('/', (req: Request, res: Response) => {
             res.send('node-ts server is running ;)');
         });
--- src/controllers/max.controller.ts
+++ src/controllers/max.controller.ts
@@ -1,10 +1,24 @@
 import * as soap from 'soap';
+import * as axios from 'axios';
+import * as http from 'http';
 
 export module MaxRequests {
-    export async function addAccount(requestData: { AccountId?: Long, Currency?: string }): Promise<any> {
-        const url: string = 'https://webservices.coriunder.cloud/v2/Prepaid.svc';
-        await soap.createClientAsync(url, {
-            forceSoap12Headers: true
-        });
+    const token: string = '';
+    const serverURL: string = 'https://webservices.coriunder.cloud/v2/Prepaid.svc';
+
+    const defaultHeaders = {
+        "Content-Type": "application/json",
+        "applicationToken": token
+    }
+
+    export async function accountAdd(requestData: { AccountId?: number, Currency?: string }) {
+        try {
+            const res: axios.AxiosResponse = await axios.default.post(
+                `${ serverURL }/AccountAdd`, requestData, { headers: defaultHeaders });
+
+            console.log(res);
+        } catch(ex) {
+            console.error(ex);
+        }
     }
 }
