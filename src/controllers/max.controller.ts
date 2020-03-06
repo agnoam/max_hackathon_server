81f713642b4eca0afaf9f04f27676fd42a144b4f
tom <zliltom@gmail.com>
2020-03-06 16:33:21 +0200

    implemention of requests 1-11

--- src/controllers/max.controller.ts
+++ src/controllers/max.controller.ts
@@ -31,8 +31,107 @@ export module MaxRequests {
     }
 
     export async function cardActivateV2(requestData: { requestParams?: PrePaidRP }): Promise<any> {
+        try {
+            const res: axios.AxiosResponse= await axios.default.post(
+                `${ serverURL }/ActivateCardV2`,requestData,{ headers:defaultHeaders});
+
+            console.log(res);
+        } catch(ex) {
+            console.error(ex);
+        }
 
     }
+
+    export async function AssignUser(requestData: { AccountId?: Long ,AccountNum?:string}): Promise<any> {
+        try {
+            const res: axios.AxiosResponse= await axios.default.post(
+                `${ serverURL }/AssignUser`,requestData,{ headers:defaultHeaders});
+
+            console.log(res);
+        } catch(ex) {
+            console.error(ex);
+        }
+
+    }
+    export async function Balance(requestData: { AccountId?: Long ,pin?:string}): Promise<any> {
+        try {
+            const res: axios.AxiosResponse= await axios.default.post(
+                `${ serverURL }/Balance`,requestData,{ headers:defaultHeaders});
+
+            console.log(res);
+        } catch(ex) {
+            console.error(ex);
+        }
+
+    }
+    export async function BlockCard(requestData: { AccountId?: Long ,PIN?:string}): Promise<any> {
+        try {
+            const res: axios.AxiosResponse= await axios.default.post(
+                `${ serverURL }/BlockCard`,requestData,{ headers:defaultHeaders});
+
+            console.log(res);
+        } catch(ex) {
+            console.error(ex);
+        }
+
+    }
+    export async function CardDebitRequest(requestData: { requestParams?: PrePaidRP }): Promise<any> {
+        try {
+            const res: axios.AxiosResponse= await axios.default.post(
+                `${ serverURL }/CardDebitRequest`,requestData,{ headers:defaultHeaders});
+
+            console.log(res);
+        } catch(ex) {
+            console.error(ex);
+        }
+
+    }
+
+    export async function CardInfo(requestData: { AccountId?: Long }): Promise<any> {
+        try {
+            const res: axios.AxiosResponse= await axios.default.post(
+                `${ serverURL }/CardInfo`,requestData,{ headers:defaultHeaders});
+
+            console.log(res);
+        } catch(ex) {
+            console.error(ex);
+        }
+
+    }
+    export async function CardIssueRequestV2(requestData: { requestParams?: PrePaidRP }): Promise<any> {
+        try {
+            const res: axios.AxiosResponse= await axios.default.post(
+                `${ serverURL }/CardIssueRequestV2`,requestData,{ headers:defaultHeaders});
+
+            console.log(res);
+        } catch(ex) {
+            console.error(ex);
+        }
+
+    }
+    export async function CardIssueRequestWithDelvAddress(requestData: { requestParams?: PrePaidRP }): Promise<any> {
+        try {
+            const res: axios.AxiosResponse= await axios.default.post(
+                `${ serverURL }/CardIssueRequestWithDelvAddress`,requestData,{ headers:defaultHeaders});
+
+            console.log(res);
+        } catch(ex) {
+            console.error(ex);
+        }
+
+    }
+    export async function CardPIN(requestData: { AccountId?: Long , ValidThru?:string , SecurityCode?:string , PIN?:string }): Promise<any> {
+        try {
+            const res: axios.AxiosResponse= await axios.default.post(
+                `${ serverURL }/CardIssueRequestWithDelvAddress`,requestData,{ headers:defaultHeaders});
+
+            console.log(res);
+        } catch(ex) {
+            console.error(ex);
+        }
+
+    }
+
 }
 
 interface CardActivateData {
